e1e21879895bc9b845a4c11af1348e780343832d
Nikhil <kumarnikhilsingh.hac07@gmail.com>
2021-02-05 14:04:22 +0530

    dry

--- app.js
+++ app.js
@@ -1,94 +1,113 @@
-let container2=document.querySelector('.container2');
-let textarea=document.getElementById('newtextarea');
-let textarea1=document.getElementById('textarea1');
+// obtaining Element
+const noteTargetArea=document.querySelector('.container2');
 const btn=document.getElementById('mybtn1');
-// let card=document.createElement('div');
+let deleteBtn=document.getElementById('btn');
+const titleArea=document.getElementById('textarea1');
+const noteArea=document.getElementById('textarea2');
+const userName=document.querySelector('.userName')
 
 
-function show(){
-    let title=localStorage.getItem('Title');
+// #defining function
+const getItemStorage=(type)=>{
+    if (type=='T') {
+        let title=localStorage.getItem('Title');
         if (title==null) {
             titleObj=[];
+            return titleObj
         } else {
-            titleObj=JSON.parse(title)
+            titleObj=JSON.parse(title);
+            return titleObj   
         }
-    let notes=localStorage.getItem('Notes');
-        if (notes==null) {
-            notesObj=[];
-        } else {
-            notesObj=JSON.parse(notes);
-        }
-        let card=""
-    notesObj.forEach((element,index) => {
-        card+=`
-        <div class="card" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title target text-center"></h5>
-          <div id="Icon" onclick="deleteNote(this.id)"><i class="fa fa-trash " aria-hidden="true"></i></div>
-          <p class="card-text">${element}</p>
-          <button id=${index} class="btn btn-primary" onclick="deleteNote(this.id)">Delete</button>
-        </div>
-      </div>
-        `
-    });
-    if (notesObj.length!=0) {
-        container2.innerHTML=card;
     } else {
-        container2.innerHTML=`<b>Noting to show here`;
+        let note=localStorage.getItem('Note');
+        if (note==null) {
+            noteObj=[];
+            return noteObj;
+        } else {
+            noteObj=JSON.parse(note);
+            return noteObj;   
+        }
     }
-    let cardHeader=document.getElementsByClassName('target');
-    titleObj.forEach((element,index)=>{
-        cardHeader[index].innerHTML=`<u>${element}</u>`
+}
+
+const setItemStorage=(text,type)=>{
+    if (type=="T") {
+        const titleArray=getItemStorage(type);
+        titleArray.push(text);
+        localStorage.setItem('Title',JSON.stringify(titleArray));
+    } 
+    else if(type=='N'){
+        const noteArray=getItemStorage(type);
+        noteArray.push(text)
+        localStorage.setItem('Note',JSON.stringify(noteArray));
+    }
+}
+
+const deleteCard=(index)=>{
+    let titleArray=getItemStorage('T');
+    let noteArray=getItemStorage('N');
+    titleArray.splice(index,1);
+    noteArray.splice(index,1);
+    localStorage.setItem('Title',JSON.stringify(titleArray));
+    localStorage.setItem('Note',JSON.stringify(noteArray));
+    show();
+
+}
+
+const show=()=>{
+    // const htmlEle=document.createElement('div');
+    let htmlEle="";
+        // htmlEle.setAttribute('style','display:inline-block;');
+        let noteMaterial=getItemStorage('N');
+        if (noteMaterial.length) {
+            noteMaterial.forEach((element,index) => {
+                htmlEle+=`<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">card1</h5>
+                  <div id=icon>
+                  <i class="fa fa-pencil text-center" aria-hidden="true"></i>
+                  <i class="fa fa-trash text-center" aria-hidden="true" onclick="deleteCard(this.id)"></i>
+                  </div>
+                  <p class="card-text">${element}</p>
+                  <button id="${index}" class="btn btn-primary secondarybtn" onclick="deleteCard(this.id)">Delete</button>
+                </div>
+              </div>`
+            noteTargetArea.innerHTML=htmlEle;
+});
+    let titleMaterial=getItemStorage('T');
+    let cardTitle=document.getElementsByClassName('card-title');
+    titleMaterial.forEach((element,index)=>{
+        cardTitle[index].innerHTML=element;
     })
 }
+else{
+    noteTargetArea.innerHTML=`<b>Nothing to show here`;
+}
+}
+    
 
-
-function deleteNote(index){
-    console.log(index);
-    let title=localStorage.getItem('Title');
-        if (title==null) {
-            titleObj=[];
-        } else {
-            titleObj=JSON.parse(title)
-        }
-    titleObj.splice(index,1)
-    localStorage.setItem('Title',JSON.stringify(titleObj));
-    let notes=localStorage.getItem('Notes');
-        if (notes==null) {
-            notesObj=[];
-        } else {
-            notesObj=JSON.parse(notes);
-        }
-    notesObj.splice(index,1);
-    localStorage.setItem('Notes',JSON.stringify(notesObj));
-    show();
+show();
+// Event addEventListener
+let storageUser=localStorage.getItem('Name');
+console.log(storageUser);
+if (storageUser==null) {
+    let name=prompt('Enter your name');
+    localStorage.setItem('Name',name);
+    userName.innerHTML=`<b><u>${storageUser}</u><b>`
+}
+else{
+    userName.innerHTML=`<b><u>${storageUser}</u><b>`
 }
 
 
-btn.addEventListener("click",()=>{
-    if (textarea.value==""||textarea1.value=="") {
-        alert('Please fill title and notes correctly');
+btn.addEventListener('click',()=>{
+    if (titleArea.value==""||noteArea.value=="") {
+        alert('Please Fill Title and Area correctly');
     } else {
-        let title=localStorage.getItem('Title');
-        let notes=localStorage.getItem('Notes');
-        if (title==null) {
-            titleObj=[];
-        } else {
-            titleObj=JSON.parse(title)
-        }
-        titleObj.push(textarea.value);
-        localStorage.setItem("Title",JSON.stringify(titleObj));
-        if (notes==null) {
-            notesObj=[];
-        } else {
-            notesObj=JSON.parse(notes);
-        }
-        notesObj.push(textarea1.value);
-        localStorage.setItem('Notes',JSON.stringify(notesObj));
-    }
-    textarea.value="";
-    textarea1.value="";
-    show();
-})
-
-show();
+          setItemStorage(titleArea.value,"T");
+          setItemStorage(noteArea.value,"N");
+          show();
+            }
+});
+         // }
+// })
